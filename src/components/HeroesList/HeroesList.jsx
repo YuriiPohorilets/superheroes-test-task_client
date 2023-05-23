cde8a8790982e82d3c0c5e4a1ba73eee5d84c4ec
YuriiPohorilets <pohorilets.y@gmail.com>
2023-05-23 18:07:38 +0300

    fixed. github actions

--- src/components/HeroesList/HeroesList.jsx
+++ src/components/HeroesList/HeroesList.jsx
@@ -1,8 +1,11 @@
+// import { useLocation } from "react-router-dom";
 import { List } from "@mui/material";
 import { HeroesItem } from "components/HeroesItem/HeroesItem";
 import { list } from "./heroListStyles";
 
 export const HeroesList = ({ heroes }) => {
+  // const location = useLocation();
+
   return (
     <List sx={list}>
       {heroes.map(({ _id, nickname, images }) => (
